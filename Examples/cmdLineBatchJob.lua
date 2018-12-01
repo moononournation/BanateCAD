8abe4ee49798c10617135c93bbe6fbcd26508bbb
Leung CHAN <cll010@ha.org.hk>
2018-12-02 01:04:19 +0800

    refine detail

--- Examples/cmdLineBatchJob.lua
+++ Examples/cmdLineBatchJob.lua
@@ -19,7 +19,7 @@ require "shape_ellipsoid"
 
 local inchesToMmConstant = 25.4
 
-function GenerateMoon(outputSize, refinementLevel, outputName, genOuterSTL)
+function GenerateMoon(outputSize, refinementLevel, outputName, alsoGenOuterSTL)
 	local extrudeSize = 0.4
 	local heightFactor = 0.025 -- suface detail height
 	local basicHeight = 0.050 -- inches
@@ -27,22 +27,25 @@ function GenerateMoon(outputSize, refinementLevel, outputName, genOuterSTL)
 	local h = (basicHeight + (outputSize * heightFactor)) * inchesToMmConstant
 	local r = ((outputSize * inchesToMmConstant) / 2) - h
 	local t = extrudeSize * shadowFactor --color map thickness
-	
+
 	local heightmap = ImageSampler({
 		-- Filename = 'Examples/map/moonBumpMap.png',
-		Filename = 'Examples/map/moonBumpAddInvertedColorMap.png',
+		-- Filename = 'Examples/map/moonBumpAddInvertedColorMap.png',
 		-- Filename = 'Examples/map/moonBumpAddInvertedColorMap5to3.png',
-		-- Filename = 'Examples/map/moonBumpAddInvertedColorMap.23k.png',
+		Filename = 'Examples/map/moonBumpAddInvertedColorMap.23k.png',
+		-- Filename = 'Examples/map/moonBumpAddInvertedColorMap5to3.23k.png',
 		Interpolate = true,
-		Blur = 1, -- smoothen surface
 	})
+	-- smoothen surface
+	heightmap.Blur = math.max(math.floor(((heightmap.Width / 10 /refinementLevel) - 1) / 2), 0)
 
 	local thicknessMap = ImageSampler({
-		Filename = 'Examples/map/moonColorMap.png',
-		-- Filename = 'Examples/map/moonColorMap.23k.png',
+		-- Filename = 'Examples/map/moonColorMap.png',
+		Filename = 'Examples/map/moonColorMap.23k.png',
 		Interpolate = true,
-		Blur = 1, -- smoothen interior shape
 	})
+	-- smoothen interior shape
+	thicknessMap.Blur = math.max(math.floor(((thicknessMap.Width / 10 /refinementLevel) - 1) / 2), 0)
 
 	local vertsampler = shape_ellipsoid({
 		XRadius = r,
@@ -76,7 +79,7 @@ function GenerateMoon(outputSize, refinementLevel, outputName, genOuterSTL)
 
 	collectgarbage()
 
-	if genOuterSTL then
+	if alsoGenOuterSTL then
 		-- generate the outer surface only object for post edit purpose
 		local lshape =  Icosahedron({
 			RefinementLevel = refinementLevel,
@@ -93,13 +96,17 @@ function GenerateMoon(outputSize, refinementLevel, outputName, genOuterSTL)
 end
 
 GenerateMoon(1.0, 96, 'moon_lamp_1_inch', false)
+GenerateMoon(1.25, 120, 'moon_lamp_1.25_inches', false)
 GenerateMoon(1.5, 144, 'moon_lamp_1.5_inches', true)
-GenerateMoon(2.0, 192, 'moon_lamp_2_inches', false)
-GenerateMoon(2.5, 240, 'moon_lamp_2.5_inches', false)
-GenerateMoon(3.0, 288, 'moon_lamp_3_inches', false)
+GenerateMoon(1.75, 168, 'moon_lamp_1.75_inches', true)
+GenerateMoon(2.0, 192, 'moon_lamp_2_inches', true)
+GenerateMoon(2.5, 240, 'moon_lamp_2.5_inches', true)
+GenerateMoon(3.0, 288, 'moon_lamp_3_inches', true)
 GenerateMoon(3.5, 336, 'moon_lamp_3.5_inches', false)
 GenerateMoon(4.0, 384, 'moon_lamp_4_inches', false)
-GenerateMoon(5.0, 480, 'moon_lamp_5_inches', false)
+GenerateMoon(5.0, 480, 'moon_lamp_5_inches', true)
 GenerateMoon(6.0, 576, 'moon_lamp_6_inches', false)
 GenerateMoon(7.0, 672, 'moon_lamp_7_inches', false)
 GenerateMoon(8.0, 768, 'moon_lamp_8_inches', false)
+
+-- GenerateMoon(24.0, 2304, 'moon_lamp_24_inches', false)
--- Examples/cmdLineBatchJob.sh
+++ Examples/cmdLineBatchJob.sh
@@ -4,7 +4,9 @@ lua Examples/cmdLineBatchJob.lua
 cd Examples
 
 # meshlabserver -i moon_lamp_1_inch.stl -o stl/moon_lamp_1_inch.stl
+# meshlabserver -i moon_lamp_1.25_inches.stl -o stl/moon_lamp_1.25_inches.stl
 # meshlabserver -i moon_lamp_1.5_inches.stl -o stl/moon_lamp_1.5_inches.stl
+# meshlabserver -i moon_lamp_1.75_inches.stl -o stl/moon_lamp_1.75_inches.stl
 # meshlabserver -i moon_lamp_2_inches.stl -o stl/moon_lamp_2_inches.stl
 # meshlabserver -i moon_lamp_2.5_inches.stl -o stl/moon_lamp_2.5_inches.stl
 # meshlabserver -i moon_lamp_3_inches.stl -o stl/moon_lamp_3_inches.stl
@@ -15,7 +17,17 @@ cd Examples
 # meshlabserver -i moon_lamp_7_inches.stl -o stl/moon_lamp_7_inches.stl
 # meshlabserver -i moon_lamp_8_inches.stl -o stl/moon_lamp_8_inches.stl
 
-# meshlabserver -i moon_lamp_1.5_inches.stl -o stl/moon_lamp_1.5_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_24_inches.stl -o stl/moon_lamp_24_inches.stl
+
 # meshlabserver -i moon_lamp_1.5_inchesOuterOnly.stl -o stl/moon_lamp_1.5_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
-# meshlabserver -i moon_lamp_5_inches.stl -o stl/moon_lamp_5_inchesY10.stl -s mlx/MeshLabRotateY10.mlx
+# meshlabserver -i moon_lamp_1.5_inches.stl -o stl/moon_lamp_1.5_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_1.75_inchesOuterOnly.stl -o stl/moon_lamp_1.75_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_1.75_inches.stl -o stl/moon_lamp_1.75_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_2_inchesOuterOnly.stl -o stl/moon_lamp_2_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_2_inches.stl -o stl/moon_lamp_2_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_2.5_inchesOuterOnly.stl -o stl/moon_lamp_2.5_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_2.5_inches.stl -o stl/moon_lamp_2.5_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_3_inchesOuterOnly.stl -o stl/moon_lamp_3_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
+# meshlabserver -i moon_lamp_3_inches.stl -o stl/moon_lamp_3_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
 # meshlabserver -i moon_lamp_5_inchesOuterOnly.stl -o stl/moon_lamp_5_inchesY10OuterOnly.stl -s mlx/MeshLabRotateY10.mlx
+# meshlabserver -i moon_lamp_5_inches.stl -o stl/moon_lamp_5_inchesY10.stl -s mlx/MeshLabRotateY10.mlx
