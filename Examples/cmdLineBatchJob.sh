97c278ac4aec2bcf35f50dff4d5c399330e83645
Leung CHAN <cll010@ha.org.hk>
2019-01-02 13:08:23 +0800

    more size

--- Examples/cmdLineBatchJob.sh
+++ Examples/cmdLineBatchJob.sh
@@ -17,7 +17,45 @@ cd Examples
 # meshlabserver -i moon_lamp_7_inches.stl -o stl/moon_lamp_7_inches.stl
 # meshlabserver -i moon_lamp_8_inches.stl -o stl/moon_lamp_8_inches.stl
 
+# meshlabserver -i moon_lamp_9_inches.stl -o stl/moon_lamp_9_inches.stl
+# meshlabserver -i moon_lamp_10_inches.stl -o stl/moon_lamp_10_inches.stl
+# meshlabserver -i moon_lamp_11_inches.stl -o stl/moon_lamp_11_inches.stl
+# meshlabserver -i moon_lamp_12_inches.stl -o stl/moon_lamp_12_inches.stl
+# meshlabserver -i moon_lamp_13_inches.stl -o stl/moon_lamp_13_inches.stl
+# meshlabserver -i moon_lamp_14_inches.stl -o stl/moon_lamp_14_inches.stl
+# meshlabserver -i moon_lamp_15_inches.stl -o stl/moon_lamp_15_inches.stl
+# meshlabserver -i moon_lamp_16_inches.stl -o stl/moon_lamp_16_inches.stl
+# meshlabserver -i moon_lamp_17_inches.stl -o stl/moon_lamp_17_inches.stl
+# meshlabserver -i moon_lamp_18_inches.stl -o stl/moon_lamp_18_inches.stl
+# meshlabserver -i moon_lamp_19_inches.stl -o stl/moon_lamp_19_inches.stl
+# meshlabserver -i moon_lamp_20_inches.stl -o stl/moon_lamp_20_inches.stl
+# meshlabserver -i moon_lamp_21_inches.stl -o stl/moon_lamp_21_inches.stl
+# meshlabserver -i moon_lamp_22_inches.stl -o stl/moon_lamp_22_inches.stl
+# meshlabserver -i moon_lamp_23_inches.stl -o stl/moon_lamp_23_inches.stl
 # meshlabserver -i moon_lamp_24_inches.stl -o stl/moon_lamp_24_inches.stl
+
+#fixed refinement level to cater Thingiverse 262144000 size limit
+# meshlabserver -i moon_lamp_4_inches_refinement_level_360.stl -o stl/moon_lamp_4_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_5_inches_refinement_level_360.stl -o stl/moon_lamp_5_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_6_inches_refinement_level_360.stl -o stl/moon_lamp_6_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_7_inches_refinement_level_360.stl -o stl/moon_lamp_7_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_8_inches_refinement_level_360.stl -o stl/moon_lamp_8_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_9_inches_refinement_level_360.stl -o stl/moon_lamp_9_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_10_inches_refinement_level_360.stl -o stl/moon_lamp_10_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_11_inches_refinement_level_360.stl -o stl/moon_lamp_11_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_12_inches_refinement_level_360.stl -o stl/moon_lamp_12_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_13_inches_refinement_level_360.stl -o stl/moon_lamp_13_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_14_inches_refinement_level_360.stl -o stl/moon_lamp_14_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_15_inches_refinement_level_360.stl -o stl/moon_lamp_15_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_16_inches_refinement_level_360.stl -o stl/moon_lamp_16_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_17_inches_refinement_level_360.stl -o stl/moon_lamp_17_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_18_inches_refinement_level_360.stl -o stl/moon_lamp_18_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_19_inches_refinement_level_360.stl -o stl/moon_lamp_19_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_20_inches_refinement_level_360.stl -o stl/moon_lamp_20_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_21_inches_refinement_level_360.stl -o stl/moon_lamp_21_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_22_inches_refinement_level_360.stl -o stl/moon_lamp_22_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_23_inches_refinement_level_360.stl -o stl/moon_lamp_23_inches_refinement_level_360.stl
+# meshlabserver -i moon_lamp_24_inches_refinement_level_360.stl -o stl/moon_lamp_24_inches_refinement_level_360.stl
 
 # meshlabserver -i moon_lamp_1.5_inchesOuterOnly.stl -o stl/moon_lamp_1.5_inchesY170OuterOnly.stl -s mlx/MeshLabRotateY170.mlx
 # meshlabserver -i moon_lamp_1.5_inches.stl -o stl/moon_lamp_1.5_inchesY170.stl -s mlx/MeshLabRotateY170.mlx
