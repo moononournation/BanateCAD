0ff488a7ae9e8ea704bec1d76c8a619ec37426e5
moononournation <moononournation@gmail.com>
2020-02-27 18:46:08 +0800

    add resin parameters

--- Examples/cmdLineBatchJob.lua
+++ Examples/cmdLineBatchJob.lua
@@ -1,7 +1,4 @@
 -- settings
-local basicWallSize = 0.6 -- mm, add a little bit on extrude size
-local shadowSize = 2.4 -- mm, larger is thicker and darker
-local constantHeightSize = 0.7 -- mm, basic surface height
 local variableHeightFactor = 0.011 -- ratio, surface height factor growth with output size
 
 require "BAppContext"
@@ -23,13 +20,12 @@ require "STLCodec"
 require "Icosahedron"
 require "shape_ellipsoid"
 
-local inchesToMmConstant = 25.4
-
-function GenerateMoon(outputSize, refinementLevel, outputName, alsoGenOuterSTL)
-	local h = constantHeightSize + (outputSize * inchesToMmConstant * variableHeightFactor)
-	local r = ((outputSize * inchesToMmConstant) / 2) - h
+function GenerateMoon(outputSize, refinementLevel, basicWallSize, shadowSize, basicHeightSize, outputName, alsoGenOuterSTL)
+	local h = basicHeightSize + (outputSize * variableHeightFactor)
+	local r = (outputSize / 2) - heightSize
 
 	local heightmap = ImageSampler({
+		-- Filename = 'Examples/map/moonBumpAddInvertedColorMap.21.6k.png',
 		Filename = 'Examples/map/moonBumpAddInvertedColorMap.3.6k.png',
 		Interpolate = true,
 	})
@@ -37,6 +33,7 @@ function GenerateMoon(outputSize, refinementLevel, outputName, alsoGenOuterSTL)
 	heightmap.Blur = math.max(math.ceil(((heightmap.Width / 10 /refinementLevel) - 1) / 2), 0)
 
 	local thicknessMap = ImageSampler({
+		-- Filename = 'Examples/map/moonColorMap.21.6k.png',
 		Filename = 'Examples/map/moonColorMap.3.6k.png',
 		Interpolate = true,
 	})
@@ -79,6 +76,9 @@ function GenerateMoon(outputSize, refinementLevel, outputName, alsoGenOuterSTL)
 		-- generate the outer surface only object for post edit purpose
 		local lshape =  Icosahedron({
 			RefinementLevel = refinementLevel,
+			-- local lshape =  BiParametric({
+			-- 	USteps = refinementLevel * 10,
+			-- 	WSteps = refinementLevel * 5,
 			VertexFunction = dispSampler,
 		})
 
@@ -91,40 +91,69 @@ function GenerateMoon(outputSize, refinementLevel, outputName, alsoGenOuterSTL)
 	end
 end
 
-GenerateMoon(1.5, 180, 'moon_lamp_1.5_inches', false)
--- GenerateMoon(2.0, 240, 'moon_lamp_2_inches', false)
--- GenerateMoon(2.5, 300, 'moon_lamp_2.5_inches', false)
--- GenerateMoon(3.0, 360, 'moon_lamp_3_inches', false)
--- GenerateMoon(3.5, 420, 'moon_lamp_3.5_inches', false)
--- GenerateMoon(4.0, 480, 'moon_lamp_4_inches', false)
--- GenerateMoon(5.0, 600, 'moon_lamp_5_inches', false)
+--[[
+outputSize:      in mm, output model diameter
+refinementLevel: number of subdivision for each edge of Icosahedron
+basicWallSize:   in mm, add a little bit on extrude size
+shadowSize:      in mm, larger is thicker and darker
+basicHeightSize: in mm, basic surface height size
+outputName:      output filename
+alsoGenOuterSTL: also generate a solid object only have surface detail for post-edit reason
+--]]
+local inchesToMmConstant = 25.4
 
--- GenerateMoon(6.0, 720, 'moon_lamp_6_inches', false)
--- GenerateMoon(7.0, 840, 'moon_lamp_7_inches', false)
--- GenerateMoon(8.0, 960, 'moon_lamp_8_inches', false)
--- GenerateMoon(9.0, 1080, 'moon_lamp_9_inches', false)
--- GenerateMoon(10.0, 1200, 'moon_lamp_10_inches', false)
--- GenerateMoon(11.0, 1320, 'moon_lamp_11_inches', false)
--- GenerateMoon(12.0, 1440, 'moon_lamp_12_inches', false)
--- GenerateMoon(13.0, 1560, 'moon_lamp_13_inches', false)
--- GenerateMoon(14.0, 1680, 'moon_lamp_14_inches', false)
--- GenerateMoon(15.0, 1800, 'moon_lamp_15_inches', false)
--- GenerateMoon(16.0, 1920, 'moon_lamp_16_inches', false)
+-- Resin 3D printing
+GenerateMoon(1.00 * inchesToMmConstant, 180, 0.1, 1.6, 0.9, 'moon_lamp_1.00_inches', false)
+-- GenerateMoon(1.25 * inchesToMmConstant, 180, 0.1, 1.6, 0.9, 'moon_lamp_1.25_inches', false)
+-- GenerateMoon(1.50 * inchesToMmConstant, 180, 0.1, 1.6, 0.9, 'moon_lamp_1.50_inches', false)
+-- GenerateMoon(1.75 * inchesToMmConstant, 180, 0.1, 1.6, 0.9, 'moon_lamp_1.75_inches', false)
+-- GenerateMoon(2.00 * inchesToMmConstant, 180, 0.1, 1.6, 0.9, 'moon_lamp_2.00_inches', false)
+
+-- FDM 3D printing
+-- GenerateMoon(2.0 * inchesToMmConstant, 180, 0.6, 2.4, 0.7, 'moon_lamp_2.0_inches', false)
+-- GenerateMoon(2.5 * inchesToMmConstant, 225, 0.6, 2.4, 0.7, 'moon_lamp_2.5_inches', false)
+-- GenerateMoon(3.0 * inchesToMmConstant, 270, 0.6, 2.4, 0.7, 'moon_lamp_3.0_inches', false)
+-- GenerateMoon(3.5 * inchesToMmConstant, 315, 0.6, 2.4, 0.7, 'moon_lamp_3.5_inches', false)
+-- GenerateMoon(4.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_4.0_inches', false)
+-- GenerateMoon(5.0 * inchesToMmConstant, 450, 0.6, 2.4, 0.7, 'moon_lamp_5.0_inches', false)
+-- GenerateMoon(6.0 * inchesToMmConstant, 540, 0.6, 2.4, 0.7, 'moon_lamp_6.0_inches', false)
+-- GenerateMoon(7.0 * inchesToMmConstant, 630, 0.6, 2.4, 0.7, 'moon_lamp_7.0_inches', false)
+-- GenerateMoon(8.0 * inchesToMmConstant, 720, 0.6, 2.4, 0.7, 'moon_lamp_8.0_inches', false)
+-- GenerateMoon(9.0 * inchesToMmConstant, 810, 0.6, 2.4, 0.7, 'moon_lamp_9.0_inches', false)
+-- GenerateMoon(10 * inchesToMmConstant,  900, 0.6, 2.4, 0.7, 'moon_lamp_10_inches', false)
+-- GenerateMoon(11 * inchesToMmConstant,  990, 0.6, 2.4, 0.7, 'moon_lamp_11_inches', false)
+-- GenerateMoon(12 * inchesToMmConstant, 1080, 0.6, 2.4, 0.7, 'moon_lamp_12_inches', false)
+-- GenerateMoon(13 * inchesToMmConstant, 1170, 0.6, 2.4, 0.7, 'moon_lamp_13_inches', false)
+-- GenerateMoon(14 * inchesToMmConstant, 1260, 0.6, 2.4, 0.7, 'moon_lamp_14_inches', false)
+-- GenerateMoon(15 * inchesToMmConstant, 1350, 0.6, 2.4, 0.7, 'moon_lamp_15_inches', false)
+-- GenerateMoon(16 * inchesToMmConstant, 1440, 0.6, 2.4, 0.7, 'moon_lamp_16_inches', false)
+-- GenerateMoon(17 * inchesToMmConstant, 1530, 0.6, 2.4, 0.7, 'moon_lamp_17_inches', false)
+-- GenerateMoon(18 * inchesToMmConstant, 1620, 0.6, 2.4, 0.7, 'moon_lamp_18_inches', false)
+-- GenerateMoon(19 * inchesToMmConstant, 1710, 0.6, 2.4, 0.7, 'moon_lamp_19_inches', false)
+-- GenerateMoon(20 * inchesToMmConstant, 1800, 0.6, 2.4, 0.7, 'moon_lamp_20_inches', false)
+-- GenerateMoon(21 * inchesToMmConstant, 1890, 0.6, 2.4, 0.7, 'moon_lamp_21_inches', false)
+-- GenerateMoon(22 * inchesToMmConstant, 1980, 0.6, 2.4, 0.7, 'moon_lamp_22_inches', false)
+-- GenerateMoon(23 * inchesToMmConstant, 2070, 0.6, 2.4, 0.7, 'moon_lamp_23_inches', false)
+-- GenerateMoon(24 * inchesToMmConstant, 2160, 0.6, 2.4, 0.7, 'moon_lamp_24_inches', false)
 
 -- fixed refinement level to cater Thingiverse 262144000 size limit
--- GenerateMoon(4.0, 360, 'moon_lamp_4_inches_refinement_level_360', false)
--- GenerateMoon(5.0, 360, 'moon_lamp_5_inches_refinement_level_360', false)
--- GenerateMoon(6.0, 360, 'moon_lamp_6_inches_refinement_level_360', false)
--- GenerateMoon(7.0, 360, 'moon_lamp_7_inches_refinement_level_360', false)
--- GenerateMoon(8.0, 360, 'moon_lamp_8_inches_refinement_level_360', false)
--- GenerateMoon(9.0, 360, 'moon_lamp_9_inches_refinement_level_360', false)
--- GenerateMoon(10, 360, 'moon_lamp_10_inches_refinement_level_360', false)
--- GenerateMoon(11, 360, 'moon_lamp_11_inches_refinement_level_360', false)
--- GenerateMoon(12, 360, 'moon_lamp_12_inches_refinement_level_360', false)
--- GenerateMoon(13, 360, 'moon_lamp_13_inches_refinement_level_360', false)
--- GenerateMoon(14, 360, 'moon_lamp_14_inches_refinement_level_360', false)
--- GenerateMoon(15, 360, 'moon_lamp_15_inches_refinement_level_360', false)
--- GenerateMoon(16, 360, 'moon_lamp_16_inches_refinement_level_360', false)
-
--- fixed refinement level to cater Thingiverse 262144000 size limit after xz
--- GenerateMoon(6, 600, 'moon_lamp_6_inches_refinement_level_600', false)
+-- GenerateMoon(5.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_5_inches_refinement_level_360', false)
+-- GenerateMoon(6.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_6_inches_refinement_level_360', false)
+-- GenerateMoon(7.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_7_inches_refinement_level_360', false)
+-- GenerateMoon(8.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_8_inches_refinement_level_360', false)
+-- GenerateMoon(9.0 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_9_inches_refinement_level_360', false)
+-- GenerateMoon(10 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_10_inches_refinement_level_360', false)
+-- GenerateMoon(11 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_11_inches_refinement_level_360', false)
+-- GenerateMoon(12 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_12_inches_refinement_level_360', false)
+-- GenerateMoon(13 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_13_inches_refinement_level_360', false)
+-- GenerateMoon(14 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_14_inches_refinement_level_360', false)
+-- GenerateMoon(15 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_15_inches_refinement_level_360', false)
+-- GenerateMoon(16 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_16_inches_refinement_level_360', false)
+-- GenerateMoon(17 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_17_inches_refinement_level_360', false)
+-- GenerateMoon(18 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_18_inches_refinement_level_360', false)
+-- GenerateMoon(19 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_19_inches_refinement_level_360', false)
+-- GenerateMoon(20 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_20_inches_refinement_level_360', false)
+-- GenerateMoon(21 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_21_inches_refinement_level_360', false)
+-- GenerateMoon(22 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_22_inches_refinement_level_360', false)
+-- GenerateMoon(23 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_23_inches_refinement_level_360', false)
+-- GenerateMoon(24 * inchesToMmConstant, 360, 0.6, 2.4, 0.7, 'moon_lamp_24_inches_refinement_level_360', false)
--- Examples/cmdLineBatchJob.sh
+++ Examples/cmdLineBatchJob.sh
@@ -2,18 +2,23 @@
 lua Examples/cmdLineBatchJob.lua
 
 mkdir -p Examples/stl
-xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.5_inches.stl -o Examples/stl/moon_lamp_1.5_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2_inches.stl -o Examples/stl/moon_lamp_2_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.5_inches.stl -o Examples/stl/moon_lamp_2.5_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_3_inches.stl -o Examples/stl/moon_lamp_3_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_3.5_inches.stl -o Examples/stl/moon_lamp_3.5_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_4_inches.stl -o Examples/stl/moon_lamp_4_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_5_inches.stl -o Examples/stl/moon_lamp_5_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.00_inches.stl -o Examples/stl/moon_lamp_1.00_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.25_inches.stl -o Examples/stl/moon_lamp_1.25_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.50_inches.stl -o Examples/stl/moon_lamp_1.50_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.75_inches.stl -o Examples/stl/moon_lamp_1.75_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.00_inches.stl -o Examples/stl/moon_lamp_2.00_inches.stl
 
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_6_inches.stl -o Examples/stl/moon_lamp_6_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_7_inches.stl -o Examples/stl/moon_lamp_7_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_8_inches.stl -o Examples/stl/moon_lamp_8_inches.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_9_inches.stl -o Examples/stl/moon_lamp_9_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.0_inches.stl -o Examples/stl/moon_lamp_2.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.5_inches.stl -o Examples/stl/moon_lamp_2.5_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_3.0_inches.stl -o Examples/stl/moon_lamp_3.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_3.5_inches.stl -o Examples/stl/moon_lamp_3.5_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_4.0_inches.stl -o Examples/stl/moon_lamp_4.0_inches.stl
+
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_5.0_inches.stl -o Examples/stl/moon_lamp_5.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_6.0_inches.stl -o Examples/stl/moon_lamp_6.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_7.0_inches.stl -o Examples/stl/moon_lamp_7.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_8.0_inches.stl -o Examples/stl/moon_lamp_8.0_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_9.0_inches.stl -o Examples/stl/moon_lamp_9.0_inches.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_10_inches.stl -o Examples/stl/moon_lamp_10_inches.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_11_inches.stl -o Examples/stl/moon_lamp_11_inches.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_12_inches.stl -o Examples/stl/moon_lamp_12_inches.stl
@@ -21,11 +26,16 @@ xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.5_in
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_14_inches.stl -o Examples/stl/moon_lamp_14_inches.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_15_inches.stl -o Examples/stl/moon_lamp_15_inches.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_16_inches.stl -o Examples/stl/moon_lamp_16_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_17_inches.stl -o Examples/stl/moon_lamp_17_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_18_inches.stl -o Examples/stl/moon_lamp_18_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_19_inches.stl -o Examples/stl/moon_lamp_19_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_20_inches.stl -o Examples/stl/moon_lamp_20_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_21_inches.stl -o Examples/stl/moon_lamp_21_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_22_inches.stl -o Examples/stl/moon_lamp_22_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_23_inches.stl -o Examples/stl/moon_lamp_23_inches.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_24_inches.stl -o Examples/stl/moon_lamp_24_inches.stl
 
 #fixed refinement level to cater Thingiverse 262144000 size limit
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_2_inches_refinement_level_360.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_3_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_3_inches_refinement_level_360.stl
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_4_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_4_inches_refinement_level_360.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_5_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_5_inches_refinement_level_360.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_6_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_6_inches_refinement_level_360.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_7_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_7_inches_refinement_level_360.stl
@@ -38,14 +48,23 @@ xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.5_in
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_14_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_14_inches_refinement_level_360.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_15_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_15_inches_refinement_level_360.stl
 # xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_16_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_16_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_17_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_17_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_18_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_18_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_19_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_19_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_20_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_20_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_21_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_21_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_22_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_22_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_23_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_23_inches_refinement_level_360.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_24_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_24_inches_refinement_level_360.stl
 
 #Moon model for placing on Desktop
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_4_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_4_inches_refinement_level_360Y180.stl -s Examples/mlx/MeshLabRotateY180.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_4.0_inches.stl -o Examples/stl/moon_lamp_4_inches_Y180.stl -s Examples/mlx/MeshLabRotateY180.mlx
 
 #Moon Model for hanging
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.5_inches.stl -o Examples/stl/moon_lamp_1.5_inchesY170.stl -s Examples/mlx/MeshLabRotateY170.mlx
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_2_inches_refinement_level_360Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_8_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_8_inches_refinement_level_360Y165.stl -s Examples/mlx/MeshLabRotateY165.mlx
-
-#fixed refinement level to cater Thingiverse 262144000 size limit after xz
-# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_6_inches_refinement_level_600.stl -o Examples/stl/moon_lamp_6_inches_refinement_level_600.stl
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.00_inches.stl -o Examples/stl/moon_lamp_1.00_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.25_inches.stl -o Examples/stl/moon_lamp_1.25_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.50_inches.stl -o Examples/stl/moon_lamp_1.50_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_1.75_inches.stl -o Examples/stl/moon_lamp_1.75_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.00_inches.stl -o Examples/stl/moon_lamp_2.00_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_2.0_inches.stl -o Examples/stl/moon_lamp_2.0_inches_Y170.stl -s Examples/mlx/MeshLabRotateY170.mlx
+# xvfb-run -a -s "-screen 0 800x600x24" meshlabserver -i Examples/moon_lamp_8_inches_refinement_level_360.stl -o Examples/stl/moon_lamp_8_inches_refinement_level_360_Y165.stl -s Examples/mlx/MeshLabRotateY165.mlx
